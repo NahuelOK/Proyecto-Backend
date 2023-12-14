5ee9ab592a94925f215d9aee5d0bf518b52cb688
Nahuel Moreno <nahuel23009@gmail.com>
2023-12-13 22:49:58 -0300

    Conecte la base de datos y se pueden crear y ver los productos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import handlebars from "express-handlebars"
+import mongoose from "mongoose";
 import __dirname from "./ultils.js"
 import productosRouter from "./routes/productos.router.js"
 
 const app = express();
+const mongoURL= "mongodb+srv://nahuel23009:6KwNRivgNXTdFnit@myfirstdb.aozrhlt.mongodb.net/"
+const mongoDBName = "proyecto-backend-2"
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views")
@@ -13,4 +19,9 @@ app.get("/", (req, res) => res.render("index", {}))
 app.get("/inicio", (req, res) => res.send("OK"))
 app.use("/productos", productosRouter)
 
-app.listen(8080, ()=> console.log("Corriendo...."))
+mongoose.connect(mongoURL, {dbName: mongoDBName})
+    .then(()=>{
+        console.log("DB conectado")
+        app.listen(8080, ()=> console.log("Corriendo...."))
+    })
+    .catch(e => console.error("Error to conect"))
--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -1,18 +1,35 @@
 import { Router } from "express";
+import productoModel from "../models/productos.models.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-    res.render("list", { productos: []})
+    const productos = await productoModel.find().lean().exec()
+    console.log({productos})
+    res.render("list", { productos })
+})
+
+router.post("/", async (req, res) => {
+    try{
+        const productoNew = req.body
+        const result = await productoModel.create(productoNew)
+        console.log({result})
+        res.redirect("/productos")
+    } catch (error){
+        console.log(error)
+        res.send("Error al crear...")
+    }
 })
 
 router.get("/create", async (req, res) => {
     res.render("create", {})
 })
 
+router.delete("/:id", async (req, res) => {
+    res.send("Producto Eliminandose...")})
+
 router.get("/:name", async (req, res) => {
     res.render("one", {producto: {
-        name:"pritty", price: 2000, stock: 23, desc: "blablabla", img:" "
     }})
 })
 
